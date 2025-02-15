3ddcfe982a1f71f9bf303f777812e4fb7e387923
Jason64364 <36782657+NewBitsNow@users.noreply.github.com>
2025-02-15 08:30:29 -0800

    Add files via upload

---
+++ main.tf
@@ -0,0 +1,29 @@
+terraform {
+  required_providers {
+    aws = {
+        source = "hashicorp/aws"
+        version = "~>4.16"
+    }
+  }
+
+  required_version = ">= 1.2.0"
+}
+
+# Configure the Provider
+provider "aws" {
+    region = "us-west-2"
+    assume_role_with_web_identity {
+        role_arn                  = "arn:aws:iam::438465144115:role/GitHubActions_PowerUser"
+        session_name              = "myWebIDSessionName"
+        web_identity_token_file   = "/Users/tf_user/secrets/web-identity-token"
+      }
+}
+
+resource "aws_vpc" "primary_vpc" {
+    cidr_block = "10.0.0/16"
+    instance_tenancy = "default"
+}
+
+resource "aws_instance" "instance_1" {
+
+}
